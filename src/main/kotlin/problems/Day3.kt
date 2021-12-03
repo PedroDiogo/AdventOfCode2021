b9b44e8c095bfac40cacc17c8f786adb2c73faaa
Pedro Diogo <158898+PedroDiogo@users.noreply.github.com>
2021-12-03 21:57:16 +0000

    Day 3, Part 2

--- src/main/kotlin/problems/Day3.kt
+++ src/main/kotlin/problems/Day3.kt
@@ -6,26 +6,66 @@ class Day3(override val input: String) : Problem {
         .map { line -> line.toCharArray().map { c -> c.digitToInt() }.toList() }
 
     override fun runPartOne(): String {
-        val sumList = Array(inputList.first().size) { _ -> 0 }
+        val gammaRate = mostCommonBitPerPosition(inputList)
+            .toInt(2)
 
-        inputList.forEach { line ->
+        val numberOfBits = inputList.first().size
+        val mask = 1.shl(numberOfBits) - 1
+        val epsilonRate = gammaRate.inv() and (mask)
+
+        return (gammaRate * epsilonRate).toString()
+    }
+
+    override fun runPartTwo(): String {
+        var list = inputList
+        var index = 0
+        while (list.size > 1) {
+            val mostCommonBit = mostCommonBitPerPosition(list)[index].digitToInt()
+            list = list.filter { number ->
+                number[index] == mostCommonBit
+            }
+            index++
+        }
+
+        val oxigenGeneratorRating = list.single().joinToString(separator = "").toInt(2)
+
+        list = inputList
+        index = 0
+        while (list.size > 1) {
+            val mostCommonBit = mostCommonBitPerPosition(list)[index].digitToInt()
+            val leastCommonBit = when (mostCommonBit) {
+                0 -> 1
+                else -> 0
+            }
+
+            list = list.filter { number ->
+                number[index] == leastCommonBit
+            }
+            index++
+        }
+
+        val co2ScrubberRating = list.single().joinToString(separator = "").toInt(2)
+        return (oxigenGeneratorRating * co2ScrubberRating).toString()
+    }
+
+    private fun mostCommonBitPerPosition(list: List<List<Int>>): String {
+        val numberOfBits = list.first().size
+        val numberOfNumbers = list.size
+        val sumList = Array(numberOfBits) { _ -> 0 }
+
+        list.forEach { line ->
             line.forEachIndexed { index, i ->
                 sumList[index] += i
             }
         }
 
-        val gammaRate = sumList
+        return sumList
             .map { i ->
                 when {
-                    i >= inputList.size / 2 -> 1
+                    2 * i >= numberOfNumbers -> 1
                     else -> 0
                 }
             }
             .joinToString(separator = "")
-            .toInt(2)
-
-        val epsilonRate = gammaRate.inv() and (1.shl(inputList.first().size) - 1)
-
-        return (gammaRate * epsilonRate).toString()
     }
 }
--- src/test/kotlin/problems/Day3Test.kt
+++ src/test/kotlin/problems/Day3Test.kt
@@ -12,5 +12,13 @@ internal class Day3Test {
     fun runPartOne(input: String, expected: String) {
         assertEquals(expected, Day3(input.replace("|","\n")).runPartOne())
     }
+
+    @ParameterizedTest
+    @CsvSource(
+        "00100|11110|10110|10111|10101|01111|00111|11100|10000|11001|00010|01010,230"
+    )
+    fun runPartTwo(input: String, expected: String) {
+        assertEquals(expected, Day3(input.replace("|","\n")).runPartTwo())
+    }
 }
 
