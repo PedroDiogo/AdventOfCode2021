cd47ff2f7b7e121adc13cafcb8dfcf2eaf54ff9f
Pedro Diogo <158898+PedroDiogo@users.noreply.github.com>
2021-12-05 18:34:56 +0000

    Day template

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,27 @@ fun main() {
         Day2("problems/day2".asResource().readText()),
         Day3("problems/day3".asResource().readText()),
         Day4("problems/day4".asResource().readText()),
-        Day5("problems/day5".asResource().readText())
+        Day5("problems/day5".asResource().readText()),
+//        Day6("problems/day6".asResource().readText()),
+//        Day7("problems/day7".asResource().readText()),
+//        Day8("problems/day8".asResource().readText()),
+//        Day9("problems/day9".asResource().readText()),
+//        Day10("problems/day10".asResource().readText()),
+//        Day11("problems/day11".asResource().readText()),
+//        Day12("problems/day12".asResource().readText()),
+//        Day13("problems/day13".asResource().readText()),
+//        Day14("problems/day14".asResource().readText()),
+//        Day15("problems/day15".asResource().readText()),
+//        Day16("problems/day16".asResource().readText()),
+//        Day17("problems/day17".asResource().readText()),
+//        Day18("problems/day18".asResource().readText()),
+//        Day19("problems/day19".asResource().readText()),
+//        Day20("problems/day20".asResource().readText()),
+//        Day21("problems/day21".asResource().readText()),
+//        Day22("problems/day22".asResource().readText()),
+//        Day23("problems/day23".asResource().readText()),
+//        Day24("problems/day24".asResource().readText()),
+//        Day25("problems/day25".asResource().readText()),
     )
 
     runProblems(problems)
---
+++ src/main/kotlin/problems/DayTemplate.kt
@@ -0,0 +1,9 @@
+package problems
+
+class DayTemplate(override val input: String) : Problem {
+    override val number: Int = 0
+
+    override fun runPartOne(): String {
+        TODO("Not yet implemented")
+    }
+}
