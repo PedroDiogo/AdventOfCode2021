756baae12c5cbabe2c87687d9e748d3cf1de603d
Pedro Diogo <158898+PedroDiogo@users.noreply.github.com>
2021-12-08 22:09:15 +0000

    Clean up Day 8

--- src/main/kotlin/problems/Day8.kt
+++ src/main/kotlin/problems/Day8.kt
@@ -5,24 +5,38 @@ class Day8(override val input: String) : Problem {
     private val entries: List<Entry> = input.lines().map { Entry.fromString(it) }
 
     override fun runPartOne(): String {
-        return entries.flatMap { entry -> entry.outputValues }
+        return entries
+            .flatMap { entry -> entry.outputValues }
             .count { outputValue -> outputValue.length in 2..4 || outputValue.length == 7 }
             .toString()
     }
 
     override fun runPartTwo(): String {
-        return entries.sumOf { entry -> entry.output() }.toString()
+        return entries
+            .sumOf { entry -> entry.output() }
+            .toString()
     }
 
     private data class Entry(val signalPatterns: List<String>, val outputValues: List<String>) {
         companion object {
             fun fromString(str: String): Entry {
-                val (signalPatterns, outputValues) = str.split("|")
+                val (signalPatterns, outputValues) = str.split(" | ")
                 return Entry(
-                    signalPatterns.split(" ").filter { it.isNotBlank() },
-                    outputValues.split(" ").filter { it.isNotBlank() })
+                    signalPatterns.split(" "),
+                    outputValues.split(" ")
+                )
             }
         }
+
+        private enum class Segment {
+            A, B, C, D, E, F, G;
+        }
+
+        val digits = mapOf(
+            1 to signalPatterns.single { pattern -> pattern.length == 2 }.toSet(),
+            4 to signalPatterns.single { pattern -> pattern.length == 4 }.toSet(),
+            7 to signalPatterns.single { pattern -> pattern.length == 3 }.toSet(),
+        )
 
         private val segmentsToDigit = mapOf(
             setOf(Segment.A, Segment.C, Segment.F, Segment.G, Segment.E, Segment.B) to '0',
@@ -36,14 +50,20 @@ class Day8(override val input: String) : Problem {
             setOf(Segment.A, Segment.C, Segment.F, Segment.G, Segment.E, Segment.B, Segment.D) to '8',
             setOf(Segment.A, Segment.C, Segment.F, Segment.G, Segment.B, Segment.D) to '9',
         )
-        private val hits = hits()
+        private val hits = signalPatterns
+            .flatMap { it.toCharArray().toList() }
+            .groupingBy { it }
+            .eachCount()
 
-        private fun hits(): Map<Char, Int> {
-            return (signalPatterns)
-                .flatMap { it.toCharArray().toList() }
-                .groupingBy { it }
-                .eachCount()
-        }
+        private val charToSegment: Map<Char, Segment> = mapOf(
+            segmentAChar() to Segment.A,
+            segmentBChar() to Segment.B,
+            segmentCChar() to Segment.C,
+            segmentDChar() to Segment.D,
+            segmentEChar() to Segment.E,
+            segmentFChar() to Segment.F,
+            segmentGChar() to Segment.G,
+        )
 
         fun output(): Int {
             return outputValues
@@ -54,32 +74,17 @@ class Day8(override val input: String) : Problem {
         }
 
         private fun valueToSegments(value: String): Set<Segment> {
-            val charToSegment = segments()
             return value.toCharArray()
                 .map { charToSegment[it]!! }
                 .toSet()
         }
 
-        private fun segments(): Map<Char, Segment> {
-            return mapOf(
-                segmentAChar() to Segment.A,
-                segmentBChar() to Segment.B,
-                segmentCChar() to Segment.C,
-                segmentDChar() to Segment.D,
-                segmentEChar() to Segment.E,
-                segmentFChar() to Segment.F,
-                segmentGChar() to Segment.G,
-            )
-        }
-
         private fun segmentAChar(): Char {
-            val digit1 = signalPatterns.single { pattern -> pattern.length == 2 }.toSet()
-            val digit7 = signalPatterns.single { pattern -> pattern.length == 3 }.toSet()
-            return (digit7 - digit1).single()
+            return (digits[7]!! - digits[1]!!).single()
         }
 
         private fun segmentBChar(): Char {
-            return hits.filter { (_, hits) -> hits == 6 }
+            return hits.filterValues { hits -> hits == 6 }
                 .keys
                 .single()
         }
@@ -91,19 +96,17 @@ class Day8(override val input: String) : Problem {
         }
 
         private fun segmentDChar(): Char {
-            val digit1 = signalPatterns.single { pattern -> pattern.length == 2 }.toSet()
-            val digit4 = signalPatterns.single { pattern -> pattern.length == 4 }.toSet()
-            return (digit4 - digit1 - segmentBChar()).single()
+            return (digits[4]!! - digits[1]!! - segmentBChar()).single()
         }
 
         private fun segmentEChar(): Char {
-            return hits.filter { (_, hits) -> hits == 4 }
+            return hits.filterValues { hits -> hits == 4 }
                 .keys
                 .single()
         }
 
         private fun segmentFChar(): Char {
-            return hits.filter { (_, hits) -> hits == 9 }
+            return hits.filterValues { hits -> hits == 9 }
                 .keys
                 .single()
         }
@@ -112,11 +115,6 @@ class Day8(override val input: String) : Problem {
             return hits.filter { (char, hits) -> hits == 7 && char != segmentDChar() }
                 .keys
                 .single()
-        }
-
-
-        private enum class Segment {
-            A, B, C, D, E, F, G;
         }
     }
 }
